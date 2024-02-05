07657d1b393be518dcc9ae6ec4ef1ebf23a9abfb
ALFI ALHUZWIRI SYAM <223970007@bukopin.co.id>
2024-02-05 11:26:51 +0700

    init

---
+++ src/test/java/com/restcicd/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restcicd.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
